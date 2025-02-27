036d027a3123fb6edc0860b9a3c63912ac596dca
Yutika Nemade <109644250+Yutika-Nemade@users.noreply.github.com>
2025-02-27 21:05:07 +0530

    memory

--- 01_basics/06_datatype-summary.js
+++ 01_basics/06_datatype-summary.js
@@ -46,3 +46,37 @@ console.log(typeof id)
 // statically typed languages:-  the data type of a variable is known at the compile time which means the programmer has to specify the data type of a variable at the time of its declaration, eg. C, C++, Java
 // dynamically types languages:- These are the languages that do not require any pre-defined data type for any variable as it is interpreted at runtime by the machine itself. In these languages, interpreters assign the data type to a variable at runtime depending on its value. eg. Javascript, python, Ruby
 
+
+// *********************** MEMORY *************************
+
+// stack used for primitive datatypes and heap used for non-primitive datatypes
+// when the value is stored in stack, we get copy of that value and when the value gets stored in heap, we get reference of that value
+
+
+// stack
+let myValue = 10
+anotherValue = 20
+
+console.log(myValue) // 10
+console.log(anotherValue) // 20
+
+// if we change the value of another variable the value of original variable will not be changed
+
+
+// Heap
+let userOne = {
+    email: "abc@gmail.com",
+    pwd: 123456
+}
+
+let anotherUser = userOne
+
+console.log(userOne) // { email: 'abc@gmail.com', pwd: 123456 }
+console.log(anotherUser)// { email: 'abc@gmail.com', pwd: 123456 }
+
+anotherUser.email = "xyz@gmail.com"
+
+console.log(userOne) //{ email: 'xyz@gmail.com', pwd: 123456 }
+console.log(anotherUser) // { email: 'xyz@gmail.com', pwd: 123456 }
+
+// if we change value of another variable, the value of original variable also gets changed
